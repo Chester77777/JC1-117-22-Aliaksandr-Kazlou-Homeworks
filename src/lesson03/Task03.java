6ee995b38b87dae1af0eee68900bfe7f0dfa7a20
Kazlou <orange0112358@gmail.com>
2022-09-12 23:12:34 +0300

    feat: update Task03

--- src/lesson03/Task03.java
+++ src/lesson03/Task03.java
@@ -3,13 +3,13 @@ package lesson03;
 public class Task03 {
     public static void main(String[] args) {
         int WhatTimeNow = 24;
-        if (WhatTimeNow < 6 | WhatTimeNow == 24) {
+        if (WhatTimeNow >= 0 && WhatTimeNow < 6 | WhatTimeNow == 24) {
             System.out.println("Good night!");
-        } else if (WhatTimeNow >= 6 & WhatTimeNow < 12) {
+        } else if (WhatTimeNow >= 6 && WhatTimeNow < 12) {
             System.out.println("Good morning!");
-        } else if (WhatTimeNow >= 12 & WhatTimeNow < 18) {
+        } else if (WhatTimeNow >= 12 && WhatTimeNow < 18) {
             System.out.println("Good day!");
-        } else if (WhatTimeNow >= 18 & WhatTimeNow < 24) {
+        } else if (WhatTimeNow >= 18 && WhatTimeNow < 24) {
             System.out.println("Good evening!");
         } else {
             System.out.println("This is not a time.");

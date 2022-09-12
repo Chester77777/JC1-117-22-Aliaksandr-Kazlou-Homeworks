bb34a991fe8a80523e98fe9d4d6eecddf7520db7
Kazlou <orange0112358@gmail.com>
2022-09-12 21:04:30 +0300

    feat: add home tasks "decision_making_statements" 1-4

---
+++ src/lesson03/Task01.java
@@ -0,0 +1,32 @@
+package lesson03;
+
+public class Task01 {
+    public static void main(String[] args) {
+        int DayOfWeek = 10;
+        switch (DayOfWeek) {
+            case 1:
+                System.out.println("This is Monday.");
+                break;
+            case 2:
+                System.out.println("This is Tuesday.");
+                break;
+            case 3:
+                System.out.println("This is Wednesday.");
+                break;
+            case 4:
+                System.out.println("This is Thursday.");
+                break;
+            case 5:
+                System.out.println("This is Friday.");
+                break;
+            case 6:
+                System.out.println("This is Saturday.");
+                break;
+            case 7:
+                System.out.println("This is Sunday.");
+                break;
+            default:
+                System.out.println("This is not a day of the week.");
+        }
+    }
+}
---
+++ src/lesson03/Task02.java
@@ -0,0 +1,24 @@
+package lesson03;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int DayOfWeek = 5;
+        if (DayOfWeek == 1) {
+            System.out.println("This is Monday.");
+        } else if (DayOfWeek == 2) {
+            System.out.println("This is Tuesday.");
+        } else if (DayOfWeek == 3) {
+            System.out.println("This is Wednesday.");
+        } else if (DayOfWeek == 4) {
+            System.out.println("This is Thursday.");
+        } else if (DayOfWeek == 5) {
+            System.out.println("This is Friday.");
+        } else if (DayOfWeek == 6) {
+            System.out.println("This is Saturday.");
+        } else if (DayOfWeek == 7){
+            System.out.println("This is Sunday.");
+        } else {
+            System.out.println("This is not a day of the week.");
+        }
+    }
+}
